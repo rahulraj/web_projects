7b19ee00181ba19e449a681c8df58e4a32104c31
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-19 20:44:01 -0400

    Added docstrings

--- assignment2/src/photogallery/tests/render_template_test.py
+++ assignment2/src/photogallery/tests/render_template_test.py
@@ -1,15 +1,23 @@
 import unittest
 from jinja2 import Environment, PackageLoader
-from ..renderers import renderer
+# Example of how to import from other parts of the package
+#from ..renderers import renderer
 
-class RenderTemplateTest(unittest.TestCase):
+class Jinja2EnvironmentTest(unittest.TestCase):
+  """ 
+  Test that Environment is set up properly with respect to
+  this package's layout. Somewhat trivial, but it was useful
+  when I was setting everything up.
+  """
   def setUp(self):
     self.environment = Environment(loader=PackageLoader('photogallery',
       'templates'))
 
-  def test_it_should_render_photo_list_given_valid_data(self):
-    template = self.environment.get_template('photo-list.html')
-    print str(template)
+  def test_environment_should_find_the_photo_list_template(self):
+    """ 
+    Get the template for the photo list. This should not throw TemplateNotFound.
+    """
+    self.environment.get_template('photo-list.html')
 
 if __name__ == '__main__':
   unittest.main()
