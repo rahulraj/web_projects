ba7cc31c528860c8fff5cd2404e38f1a1b917cb3
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-21 18:06:06 -0400

    Added a test to show that it ignores non jpeg files

--- assignment2/src/photogallery/tests/gallery_item_factory_test.py
+++ assignment2/src/photogallery/tests/gallery_item_factory_test.py
@@ -11,7 +11,6 @@ class SimpleStubOsModule(object):
     
     #return ['6170_sample_image_01.jpg', '6170_sample_image_02.jpg'] 
 class GalleryItemFactoryTest(unittest.TestCase):
-
   def setUp(self):
     self.images = ['6170_sample_image_01.jpg', '6170_sample_image_02.jpg'] 
     self.factory = GalleryItemFactory(None, None,
@@ -23,4 +22,9 @@ class GalleryItemFactoryTest(unittest.TestCase):
     for picture in jpeg_pictures:
       self.assertTrue(picture.get_file_name() in self.images)
 
-  
+  def test_it_should_ignore_non_jpeg_files(self):
+    self.images.append('not_a_jpeg.txt')
+    jpeg_directory = self.factory.create_directory('/not/real')
+    jpeg_pictures = jpeg_directory.get_contents()
+    picture_filenames = [picture.get_file_name() for picture in jpeg_pictures]
+    self.assertTrue('not_a_jpeg.txt' not in picture_filenames)
