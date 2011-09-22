8bd1c493c874f2a09463188f8a0c6ee17b1e01b0
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-21 22:06:40 -0400

    Wrote code to parse options from a json file

--- assignment2/src/photogallery/generator/manifestparser.py
+++ assignment2/src/photogallery/generator/manifestparser.py
@@ -1,4 +1,14 @@
 import simplejson as json
+from ..utils.inject import assign_injectables
 
 def parse_manifest_data(to_parse):
   return json.loads(to_parse)
+
+class ManifestParser(object):
+  def __init__(self, input_file):
+    assign_injectables(self, locals())
+
+  def get_json_data(self):
+    lines = self.input_file.readlines()
+    json_string = "\n".join(lines)
+    return parse_manifest_data(json_string)
--- assignment2/src/photogallery/tests/manifest_parser_test.py
+++ assignment2/src/photogallery/tests/manifest_parser_test.py
@@ -1,8 +1,8 @@
 import unittest
-from ..generator.manifestparser import parse_manifest_data
+from ..generator.manifestparser import parse_manifest_data, ManifestParser
 
 class ManifestParserTest(unittest.TestCase):
-  def test_it_should_extract_data_from_the_json_object(self):
+  def test_it_should_extract_data_from_the_json_text(self):
     to_parse = '{"Photographer": 80,\n' + \
                '"City": 90,\n' + \
                '"Country": 101,\n' + \
@@ -12,6 +12,16 @@ class ManifestParserTest(unittest.TestCase):
     self.assertTrue(result['City'] == 90)
     self.assertTrue(result['Country'] == 101)
     self.assertTrue(result['Description'] == 120)
+
+class ParserIntegrationTest(unittest.TestCase):
+  def test_it_should_extract_data_from_the_json_from_file(self):
+    with open('manifest.json') as input_file:
+      parser = ManifestParser(input_file)
+      result = parser.get_json_data()
+      self.assertTrue(result['Photographer'] == 80)
+      self.assertTrue(result['City'] == 90)
+      self.assertTrue(result['Country'] == 101)
+      self.assertTrue(result['Description'] == 120)
 
 if __name__ == '__main__':
   unittest.main()
