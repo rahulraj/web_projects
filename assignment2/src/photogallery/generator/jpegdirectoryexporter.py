f37fda61af802e294a94ba95d8a492fcd1d8e402
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-22 17:29:29 -0400

    Simplified the exporter

--- assignment2/src/photogallery/generator/jpegdirectoryexporter.py
+++ assignment2/src/photogallery/generator/jpegdirectoryexporter.py
@@ -1,30 +1,33 @@
+from jinja2 import Environment, PackageLoader
 from ..utils.inject import assign_injectables
 
 PHOTO_DIRECTORY_TEMPLATE_NAME = 'photo-directory.html'
 PHOTO_DETAIL_TEMPLATE_NAME = 'photo-detail.html'
 
-
-class JpegDirectoryExporter(object):
-  """
-  Class to take a JpegDirectory and export it as a HTML
-  file using Jinja2 templates. Each directory will have
-  its own page, and the pages will be nested the same way
-  the directories are nested on the user's filesystem.
-
-  Args:
-    jinja_template - the Template to populate
-  """
+class Exporter(object):
   def __init__(self, jinja_template):
+    """
+    Constructor for an exporter. It can export either
+    a JpegDirectory or a JpegPicture.
+
+    Args:
+      jinja_template - the Template to populate
+    """
     assign_injectables(self, locals())
 
-  def export_directory(self, jpeg_directory_view):
-    return self.jinja_template.render(jpeg_directory_view)
+  def export(self, view):
+    """
+    Given an ImmutableDict with which to populate the template,
+    render the template and return it.
+    """
+    return self.jinja_template.render(view)
 
+def create_photo_detail_exporter():
+  return create_exporter(PHOTO_DETAIL_TEMPLATE_NAME)
 
-class JpegPictureExporter(object):
-  """
-  Exports the details of a single JPEG picture as a
-  HTML file, using Jinja2 templates. All JPEGs have
-  a detail page.
-  """
-  pass
+def create_photo_directory_exporter():
+  return create_exporter(PHOTO_DIRECTORY_TEMPLATE_NAME)
+
+def create_exporter(template_name):
+  environment = Environment(loader=PackageLoader('photogallery', 'templates'))
+  return Exporter(environment.get_template(template_name))
--- assignment2/src/photogallery/tests/jpeg_directory_exporter_test.py
+++ assignment2/src/photogallery/tests/jpeg_directory_exporter_test.py
@@ -1,7 +1,7 @@
 import unittest
 from ..utils.inject import assign_injectables
 from ..utils.immutabledict import ImmutableDict
-from ..generator.jpegdirectoryexporter import JpegDirectoryExporter
+from ..generator.jpegdirectoryexporter import Exporter
 
 class MockJinja2Template(object):
   def __init__(self, required_values):
@@ -11,7 +11,7 @@ class MockJinja2Template(object):
     for argument in template_arguments:
       assert (argument in self.required_values)
 
-class JpegDirectoryExporterTest(unittest.TestCase):
+class ExporterTest(unittest.TestCase):
   def setUp(self):
     self.required_values = ['title', 'images']
     self.mock_template = MockJinja2Template(self.required_values)
@@ -20,10 +20,10 @@ class JpegDirectoryExporterTest(unittest.TestCase):
     self.images = [picture]
     self.directory_view = ImmutableDict.of(title='My Pictures',
         images=self.images)
-    self.exporter = JpegDirectoryExporter(self.mock_template)
+    self.exporter = Exporter(self.mock_template)
 
   def test_it_should_populate_the_jinja2_template(self):
-    self.exporter.export_directory(self.directory_view)
+    self.exporter.export(self.directory_view)
 
 if __name__ == '__main__':
   unittest.main()
