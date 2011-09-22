895a22b4fb4fa409a2d02a9e9982f1b9fe16537b
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-22 18:21:09 -0400

    Added the help message

--- assignment2/src/photogallery/generator/gallerygenerator.py
+++ assignment2/src/photogallery/generator/gallerygenerator.py
@@ -1,6 +1,12 @@
+import getopt
+import sys
 from ..utils.inject import assign_injectables
 
 class GalleryGenerator(object):
+  """
+  The top level class for the application. This is the only object
+  that the main function interacts with.
+  """
   def __init__(self, gallery_item_factory, path, exporter):
     """
     Constructor for GalleryGenerator
@@ -13,10 +19,38 @@ class GalleryGenerator(object):
     assign_injectables(self, locals())
 
   @classmethod
-  def create_gallery_generator(command_line_arguments):
-    pass
+  def create_gallery_generator(clazz, command_line_arguments):
+    """
+    Given command line arguments, wire up the application and return
+    it to the main function.
+
+    Acceptable command line arguments are:
+    -h, --help -> Prints a help message
+    -i, --input-directory -> The root directory for the gallery (required)
+    -o, --output-directory -> the output directory for the HTML (required)
+
+    Args:
+      command_line_arguments the command line args with the program
+                             name removed.
+    """
+    try:
+      opts, args = getopt.getopt(command_line_arguments, "")
+    except getopt.GetoptError:
+      clazz.print_usage()
+      sys.exit(2)
+
+  @classmethod
+  def print_usage(clazz):
+    print "Please call this script with the following arguments:"
+    print "-i my_pictures/ where my_pictures is the directory containing " + \
+        "the JPEGs to render (long form: --input-file)"
+    print "-o my_site/ where my_site is the directory in which to " + \
+        "write the output files (long form: --output-file)"
+    print "Calling this script with -h or --help prints this message " + \
+        "and exits."
+
 
   def run(self):
     top_directory = self.gallery_item_factory.create_directory(self.path)
     directory_view = top_directory.as_directory_view()
-    populated_template = self.exporter.export(directory_view)
+    populated_templates = self.exporter.export(directory_view)
