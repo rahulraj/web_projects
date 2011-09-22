51c7c8121bcf691b3ded57f65cc8a30dee823973
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-21 20:14:45 -0400

    Stubbed out jpegdirectoryexporter

--- assignment2/src/photogallery/generator/jpegdirectoryexporter.py
+++ assignment2/src/photogallery/generator/jpegdirectoryexporter.py
@@ -1,3 +1,5 @@
+from ..utils.inject import assign_injectables
+
 class JpegDirectoryExporter(object):
   """
   Class to take a JpegDirectory and export it as a HTML
@@ -5,3 +7,5 @@ class JpegDirectoryExporter(object):
   its own page, and the pages will be nested the same way
   the directories are nested on the user's filesystem.
   """
+  def __init__(self, jinja_environment):
+    assign_injectables(self, locals())
