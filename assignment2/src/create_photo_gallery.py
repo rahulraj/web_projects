0c589753b6096693889ad852d56c1990b371eae8
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-22 21:09:26 -0400

    Filled in templatewriter

--- assignment2/src/create_photo_gallery.py
+++ assignment2/src/create_photo_gallery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env/python
 import sys
-from photogallery.generator.gallerygenerator import GalleryGenerator
+from photogallery.generator import gallerygenerator
 
 if __name__ == '__main__':
   """
   Main function for the whole application.
   """
-  generator = GalleryGenerator.create_gallery_generator(sys.argv[1:])
+  generator = gallerygenerator.create_gallery_generator(sys.argv[1:])
   generator.run()
-  pass
--- assignment2/src/photogallery/generator/templatewriter.py
+++ assignment2/src/photogallery/generator/templatewriter.py
@@ -7,15 +7,37 @@ class TemplateWriter(object):
   Class to write populated templates to the disk.
   """
   def __init__(self, output_directory):
+    """
+    Constructor for TemplateWriter
+
+    Args:
+      output_directory the directory in which to place output files.
+    """
     assign_injectables(self, locals())
 
-  def write_templates(templates):
-    pass
+  def write_templates(self, templates):
+    """
+    Given a list of HtmlFileNameAndContents, create files for all of them
+    and write the populated templates into the files.
+
+    Args:
+      templates the HtmlFileNameAndContents describing the files to be made.
+
+    Effects:
+      Performs IO by writing multiple files to disk.
+    """
+    for template in templates:
+      output_file = self.output_directory + template.get_name()
+      with open(output_file, 'w') as template_out_file:
+        template_out_file.write(template.get_contents())
 
 def create_template_writer(directory_name):
   """
   Factory function for TemplateWriter, ensures that the
   directory exists, and creates it if necessary.
+
+  Args:
+      directory_name the name of the directory to place output files.
   """
   if not os.path.isdir(directory_name):
     os.makedirs(directory_name)
