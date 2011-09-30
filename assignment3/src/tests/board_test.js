7a79baa1b9a20a9617c341146a6abb86adefa231
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-30 15:41:37 -0400

    Fixed the board tests

--- assignment3/src/tests/board_test.js
+++ assignment3/src/tests/board_test.js
@@ -1,6 +1,4 @@
-// Dependencies: board.js
-
-BoardTest = TestCase("Board");
+BoardTest = TestCase('BoardTest');
 
 BoardTest.prototype.testCorrectDimensions = function() {
   assertEquals(8, othello.Board.size);
@@ -10,17 +8,17 @@ BoardTest.prototype.testCreateRow = function() {
   /** @const */ var row = othello.Board.Builder.createRow();
   assertEquals(othello.Board.size, row.length);
   for (var i = 0; i < othello.Board.size; i++) {
-    assertEquals(othello.Board.Piece.empty, row[i]);
+    assertEquals(othello.Piece.empty, row[i]);
   }
 };
 
 BoardTest.prototype.testInitialGame = function() {
-  /** @const */ var board = othello.Board.Builder.initialGame();
+  /** @const */ var board = othello.Board.Builder.initialGame().build();
 
-  assertEquals(othello.Board.Piece.light, board.pieceAt(3, 3));
-  assertEquals(othello.Board.Piece.dark, board.pieceAt(3, 4));
-  assertEquals(othello.Board.Piece.dark, board.pieceAt(4, 3));
-  assertEquals(othello.Board.Piece.light, board.pieceAt(4, 4));
+  assertEquals(othello.Piece.light, board.pieceAt(3, 3));
+  assertEquals(othello.Piece.dark, board.pieceAt(3, 4));
+  assertEquals(othello.Piece.dark, board.pieceAt(4, 3));
+  assertEquals(othello.Piece.light, board.pieceAt(4, 4));
 }
 
 BoardTest.prototype.testBuilderCreation = function() {
@@ -29,7 +27,7 @@ BoardTest.prototype.testBuilderCreation = function() {
     at(3, 5).placeDarkMarker().
     at(6, 7).placeLightMarker().build()
 
-  assertEquals(othello.Board.Piece.light, board.pieceAt(1, 1));
-  assertEquals(othello.Board.Piece.dark, board.pieceAt(3, 5));
-  assertEquals(othello.Board.Piece.light, board.pieceAt(6, 7));
+  assertEquals(othello.Piece.light, board.pieceAt(1, 1));
+  assertEquals(othello.Piece.dark, board.pieceAt(3, 5));
+  assertEquals(othello.Piece.light, board.pieceAt(6, 7));
 };
