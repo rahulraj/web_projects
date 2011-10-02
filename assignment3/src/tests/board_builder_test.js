d695828e0d7122feb2403d3984d821adcdf3461f
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-01 21:51:27 -0400

    Moved the builder tests to their own file

---
+++ assignment3/src/tests/board_builder_test.js
@@ -0,0 +1,45 @@
+BoardBuilderTest = TestCase('BoardBuilderTest');
+
+
+BoardBuilderTest.prototype.testCorrectDimensions = function() {
+  assertEquals(8, othello.Board.size);
+};
+
+BoardBuilderTest.prototype.testCreateRow = function() {
+  /** @const */ var row = othello.Board.Builder.createRow();
+  assertEquals(othello.Board.size, row.length);
+  _.each(_.range(0, othello.Board.size), function(i) {
+    assertEquals(othello.EmptyPiece.instance, row[i]);
+  });
+};
+
+
+BoardBuilderTest.prototype.testInitialGame = function() {
+  /** @const */ var board = othello.Board.Builder.initialGame().build();
+
+  assertEquals(othello.LightPiece.instance, board.pieceAt(3, 3));
+  assertEquals(othello.DarkPiece.instance, board.pieceAt(3, 4));
+  assertEquals(othello.DarkPiece.instance, board.pieceAt(4, 3));
+  assertEquals(othello.LightPiece.instance, board.pieceAt(4, 4));
+};
+
+BoardBuilderTest.prototype.testBuilderCreation = function() {
+  /** @const */ var board = othello.Board.Builder.emptyBoard().
+      at(1, 1).placeLightMarker().
+      at(3, 5).placeDarkMarker().
+      at(6, 7).placeLightMarker().build();
+
+  assertEquals(othello.LightPiece.instance, board.pieceAt(1, 1));
+  assertEquals(othello.DarkPiece.instance, board.pieceAt(3, 5));
+  assertEquals(othello.LightPiece.instance, board.pieceAt(6, 7));
+};
+
+BoardBuilderTest.prototype.testBuilderTemplate = function() {
+  /** @const */ var first = othello.Board.Builder.initialGame().build();
+  /** @const */ var second = othello.Board.Builder.templatedBy(first).
+      at(1, 1).placeLightMarker().build();
+
+  assertEquals(second.pieceAt(1, 1), othello.LightPiece.instance);
+  assertEquals('The original board should be unchanged',
+      first.pieceAt(1, 1), othello.EmptyPiece.instance);
+};
