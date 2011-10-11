1e3fdfc2fe548e0d122aa771a0218f2f70861714
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-11 12:14:32 -0400

    Added docs for GameStartForm

--- assignment3/src/scripts/gameStartForm.js
+++ assignment3/src/scripts/gameStartForm.js
@@ -1,12 +1,29 @@
+/**
+ * Constructor for a GameStartForm
+ * @param {jQuery} formElement the jQuery element wrapping the form.
+ * @param {jQuery} playButton the jQuery element wrapping the
+ *     button that starts the game.
+ * @constructor
+ */
 othello.GameStartForm = function(formElement, playButton) {
   /** @const */ this.formElement = formElement;
   /** @const */ this.playButton = playButton;
 };
 
+/**
+ * Attach this form to a parent element, presumably to add it to
+ * the document being displayed
+ * @param {jQuery} parentElement the parent for this form.
+ */
 othello.GameStartForm.prototype.attachTo = function(parentElement) {
   parentElement.append(this.formElement);
 };
 
+/**
+ * Factory function to create a default form for Othello.
+ * @return {othello.GameStartForm} a form asking who will play
+ *     each side.
+ */
 othello.GameStartForm.createDefaultStartForm = function() {
   return new othello.GameStartForm.Builder().
     whitePlayerFieldset().
@@ -15,11 +32,21 @@ othello.GameStartForm.createDefaultStartForm = function() {
     build();
 };
 
+/**
+ * Constructor for a Builder for GameStartForms. Using a Builder
+ * instead of hard coding the elements makes the form more
+ * flexible.
+ * @constructor
+ */
 othello.GameStartForm.Builder = function() {
   /** @const */ this.formElement = $('<form>');
   this.playButtonElement = othello.utils.None.instance;
 };
 
+/**
+ * Add a fieldset to ask who will play white
+ * @return {othello.GameStartForm.Builder} the Builder for chaining.
+ */
 othello.GameStartForm.Builder.prototype.whitePlayerFieldset = function() {
   /** @const */ var whiteFieldset =
       othello.PlayerFieldset.createWhitePlayerFieldset();
@@ -27,6 +54,10 @@ othello.GameStartForm.Builder.prototype.whitePlayerFieldset = function() {
   return this;
 };
 
+/**
+ * Add a fieldset to ask who will play black
+ * @return {othello.GameStartForm.Builder} the Builder for chaining.
+ */
 othello.GameStartForm.Builder.prototype.blackPlayerFieldset = function() {
   /** @const */ var blackFieldset =
       othello.PlayerFieldset.createBlackPlayerFieldset();
@@ -34,6 +65,10 @@ othello.GameStartForm.Builder.prototype.blackPlayerFieldset = function() {
   return this;
 };
 
+/**
+ * Add a button to start the game.
+ * @return {othello.GameStartForm.Builder} the Builder for chaining.
+ */
 othello.GameStartForm.Builder.prototype.playButton = function() {
   this.playButtonElement = $('<input>', {
       type: 'button',
@@ -43,27 +78,54 @@ othello.GameStartForm.Builder.prototype.playButton = function() {
   return this;
 };
 
+/**
+ * Build the form
+ * @return {othello.GameStartForm} the newly built form.
+ */
 othello.GameStartForm.Builder.prototype.build = function() {
   return new othello.GameStartForm(this.formElement, this.playButtonElement);
 };
 
 
+/**
+ * Class to represent a fieldset to choose who will play a side.
+ * @param {jQuery} fieldsetElement the element containing the
+ *     fieldset, as a jQuery object.
+ * @constructor
+ */
 othello.PlayerFieldset = function(fieldsetElement) {
   /** @const */ this.fieldsetElement = fieldsetElement;
 };
 
+/**
+ * Attach this fieldset to a parent 
+ * @param {jQuery} parentElement the parent for this fieldset.
+ */
 othello.PlayerFieldset.prototype.attachTo = function(parentElement) {
   parentElement.append(this.fieldsetElement);
 };
 
+/**
+ * Factory function to create a default fieldset for White
+ * @return {othello.PlayerFieldset} a new fieldset.
+ */
 othello.PlayerFieldset.createWhitePlayerFieldset = function() {
   return othello.PlayerFieldset.createPlayerFieldset('White Player', 'white');
 };
 
+/**
+ * Factory function to create a default fieldset for Black
+ * @return {othello.PlayerFieldset} a new fieldset.
+ */
 othello.PlayerFieldset.createBlackPlayerFieldset = function() {
   return othello.PlayerFieldset.createPlayerFieldset('Black Player', 'black');
 };
 
+/**
+ * Factory function to create an arbitrary player fieldset
+ * @param {string} legendName the name for the legend element.
+ * @param {string} color either 'white' or 'black'. 
+ */
 othello.PlayerFieldset.createPlayerFieldset = function(legendName, color) {
   return new othello.PlayerFieldset.Builder().
       legend(legendName).
@@ -78,6 +140,7 @@ othello.PlayerFieldset.createPlayerFieldset = function(legendName, color) {
  * methods to build the fieldset. Note that order does matter,
  * the order in which methods are called will be the order in
  * which elements are displayed in the resulting document.
+ * @constructor
  */
 othello.PlayerFieldset.Builder = function() {
   /** @const */ this.fieldsetElement = $('<fieldset>');
@@ -102,6 +165,8 @@ othello.PlayerFieldset.Builder.prototype.legend = function(legendName) {
  *     radioButton('whiteHuman').checked().withLabel('Human').
  *     radioButton('whiteEasyAi').withLabel('Easy AI').
  *     radioButton('whiteMediumAi').withLabel('Medium AI').build();
+ * TODO Name some of the anonymous objects to make the code cleaner.
+ * @param {string} buttonName the name of the button.
  */
 othello.PlayerFieldset.Builder.prototype.radioButtonSetNamed =
     function(buttonName) {
