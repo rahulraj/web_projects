1f940e977f64231bcefb43aba8277d2ccc927cd5
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-10 23:43:33 -0400

    Wrote code to programatically generate the form

--- assignment3/src/scripts/gameStartForm.js
+++ assignment3/src/scripts/gameStartForm.js
@@ -1,7 +1,133 @@
-othello.GameStartForm = function() {
+othello.GameStartForm = function(formElement, playButton) {
+  /** @const */ this.formElement = formElement;
+  /** @const */ this.playButton = playButton;
+};
 
-}
+othello.GameStartForm.prototype.attachTo = function(parentElement) {
+  parentElement.append(this.formElement);
+};
 
-othello.GameStartForm.Builder = function(parentElement) {
+othello.GameStartForm.createDefaultStartForm = function() {
+  return new othello.GameStartForm.Builder().
+    whitePlayerFieldset().
+    blackPlayerFieldset().
+    playButton().
+    build();
+};
 
+othello.GameStartForm.Builder = function() {
+  /** @const */ this.formElement = $('<form>');
+  this.playButtonElement = othello.utils.None.instance;
+};
+
+othello.GameStartForm.Builder.prototype.whitePlayerFieldset = function() {
+  /** @const */ var whiteFieldset =
+      othello.PlayerFieldset.createWhitePlayerFieldset();
+  whiteFieldset.attachTo(this.formElement);
+  return this;
+};
+
+othello.GameStartForm.Builder.prototype.blackPlayerFieldset = function() {
+  /** @const */ var blackFieldset =
+      othello.PlayerFieldset.createBlackPlayerFieldset();
+  blackFieldset.attachTo(this.formElement);
+  return this;
+};
+
+othello.GameStartForm.Builder.prototype.playButton = function() {
+  this.playButtonElement = $('<input>', {
+      type: 'button',
+      value: 'Play!'
+  });
+  this.formElement.append(this.playButtonElement);
+  return this;
+};
+
+othello.GameStartForm.Builder.prototype.build = function() {
+  return new othello.GameStartForm(this.formElement, this.playButtonElement);
+};
+
+
+othello.PlayerFieldset = function(fieldsetElement) {
+  /** @const */ this.fieldsetElement = fieldsetElement;
+};
+
+othello.PlayerFieldset.prototype.attachTo = function(parentElement) {
+  parentElement.append(this.fieldsetElement);
+};
+
+othello.PlayerFieldset.createWhitePlayerFieldset = function() {
+  return othello.PlayerFieldset.createPlayerFieldset('White Player', 'white');
+};
+
+othello.PlayerFieldset.createBlackPlayerFieldset = function() {
+  return othello.PlayerFieldset.createPlayerFieldset('Black Player', 'black');
+};
+
+othello.PlayerFieldset.createPlayerFieldset = function(legendName, color) {
+  return new othello.PlayerFieldset.Builder().
+      legend(legendName).
+      radioButtonSetNamed(color + 'Player').
+          radioButton(color + 'Human').withLabel('Human').
+          radioButton(color + 'EasyAi').withLabel('Easy AI').
+          radioButton(color + 'MediumAi').withLabel('Medium AI').build();
+};
+
+/**
+ * Initialize a PlayerFieldset Builder. Call the provided
+ * methods to build the fieldset. Note that order does matter,
+ * the order in which methods are called will be the order in
+ * which elements are displayed in the resulting document.
+ */
+othello.PlayerFieldset.Builder = function() {
+  /** @const */ this.fieldsetElement = $('<fieldset>');
+};
+
+/**
+ * Add a legend to the top.
+ * @param {string} legendName the name for the legend.
+ * @return {othello.PlayerFieldset.Builder} this Builder for
+ *     chaining.
+ */
+othello.PlayerFieldset.Builder.prototype.legend = function(legendName) {
+  /** @const */ var legendHtml = $('<legend>', {html: legendName});
+  this.fieldsetElement.append(legendHtml);
+  return this;
+};
+
+/**
+ * Creates a set of radio buttons. Uses nested closures to provide
+ * a more fluent interface. Usage:
+ * builder.radioButtonSetNamed('whitePlayer').
+ *     radioButton('whiteHuman').withLabel('Human').
+ *     radioButton('whiteEasyAi').withLabel('Easy AI').
+ *     radioButton('whiteMediumAi').withLabel('Medium AI').build();
+ */
+othello.PlayerFieldset.Builder.prototype.radioButtonSetNamed =
+    function(buttonName) {
+  /** @const */ var fieldsetElement = this.fieldsetElement;
+  /** @const */ var radioButtonBuilder = {
+      radioButton: function(buttonId) {
+        return {
+          withLabel: function(labelHtml) {
+            /** @const */ var radioElement = $('<input>', {
+                type: 'radio',
+                name: buttonName,
+                id: buttonId
+            });
+            /** @const */ var labelElement = $('<label>', {
+                'for': buttonId,
+                html: labelHtml
+            });
+            fieldsetElement.append(radioElement).append(labelElement);
+            return radioButtonBuilder;
+          },
+        };
+      },
+          
+      build: function() {
+        return new othello.PlayerFieldset(fieldsetElement);
+      }
+  };
+  return radioButtonBuilder;
 };
