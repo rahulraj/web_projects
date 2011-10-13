79cf22700feed5c827932224268bc606540ef644
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-13 16:50:07 -0400

    Fixed a litner error

--- assignment3/src/scripts/observer.js
+++ assignment3/src/scripts/observer.js
@@ -9,7 +9,7 @@ othello.Observer = function() {};
 
 /**
  * Call this method to update the observer about a board change.
- * @param {othello.Board} the newly changed board.
- * @param {othello.Player} the player who just moved.
+ * @param {othello.Board} board the newly changed board.
+ * @param {othello.Player} playerWhoMoved the player who just moved.
  */
 othello.Observer.prototype.onModelChange = function(board, playerWhoMoved) {};
