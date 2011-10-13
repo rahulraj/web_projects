ec3489df7e0a0294c0efff1b5a70dea6ab6233f7
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-13 16:51:59 -0400

    Added observe functionality to undoStack

--- assignment3/src/scripts/undoStack.js
+++ assignment3/src/scripts/undoStack.js
@@ -4,6 +4,8 @@
 /**
  * A class to keep track of old boards for the undo feature.
  * @constructor
+ * @implements {othello.Observer}
+ * @const
  */
 othello.UndoStack = function() {
   this.boards = [];
@@ -13,6 +15,7 @@ othello.UndoStack = function() {
 /**
  * Push a board onto the stack
  * @param {othello.Board} board the board to push.
+ * @const
  */
 othello.UndoStack.prototype.push = function(board) {
   this.boards.push(board);
@@ -22,6 +25,7 @@ othello.UndoStack.prototype.push = function(board) {
 /**
  * Remove a board from the stack and return it.
  * @return {othello.Board} the popped board.
+ * @const
  */
 othello.UndoStack.prototype.pop = function() {
   return this.boards.pop();
@@ -31,7 +35,19 @@ othello.UndoStack.prototype.pop = function() {
 /**
  * Verify that there are boards in the stack.
  * @return {boolean} whether or not it's safe to call pop.
+ * @const
  */
 othello.UndoStack.prototype.hasBoards = function() {
   return this.boards.length > 0;
+};
+
+
+/**
+ * When the board changes, push the new board onto the stack.
+ * @param {othello.Board} board the newly changed board.
+ * @param {othello.Player} unused_playerWhoMoved the player who just moved.
+ */
+othello.UndoStack.prototype.onModelChange =
+    function(board, unused_playerWhoMoved) {
+  this.push(board);
 };
