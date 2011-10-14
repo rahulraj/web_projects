5f5a59c3041957c51ad197c98e640789c5031f2d
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-14 17:12:17 -0400

    Wrote boardHistory.js

---
+++ assignment3/src/scripts/boardHistory.js
@@ -0,0 +1,69 @@
+
+
+
+/**
+ * A class to keep track of old boards for the undo/redo feature.
+ * @constructor
+ * @const
+ */
+othello.BoardHistory = function() {
+  this.boards = [];
+  this.boardIndex = 0;
+};
+
+
+/**
+ * Push a board onto the stack. This means throwing out
+ * any redos in front of the current board.
+ * @param {othello.Board} board the board to push.
+ * @const
+ */
+othello.BoardHistory.prototype.push = function(board) {
+  /** @const */ var self = this;
+  this.boards = _.map(_.range(0, this.boardIndex + 1), function(index) {
+    return self.boards[index];
+  });
+  this.boards.push(board);
+  this.boardIndex++;
+};
+
+
+/**
+ * Move back by one board. Return the board pointed at.
+ * @return {othello.Board} the previous board.
+ * @const
+ */
+othello.BoardHistory.prototype.undo = function() {
+  this.boardIndex--;
+  return this.boards[this.boardIndex];
+};
+
+
+/**
+ * Verify that there are boards in the stack.
+ * @return {boolean} whether or not it's safe to undo.
+ * @const
+ */
+othello.BoardHistory.prototype.canUndo = function() {
+  return this.boardIndex > 0;
+};
+
+
+/**
+ * Move forward by one baord.
+ * @return {othello.Board} the redone board.
+ * @const
+ */
+othello.BoardHistory.prototype.redo = function() {
+  this.boardIndex++;
+  return this.boards[this.boardIndex];
+};
+
+
+/**
+ * Verify that it's possible to redo
+ * @return {boolean} whether it's safe  to redo.
+ */
+othello.BoardHistory.prototype.canRedo = function() {
+  return (this.boardIndex + 1) < this.boards.length;
+};
