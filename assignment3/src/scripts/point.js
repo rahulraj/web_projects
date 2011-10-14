89bcd0ed0da18d3aa9c4f63344550304a6aea9c8
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-14 16:43:31 -0400

    Wrote code to enforce immutability

--- assignment3/src/scripts/point.js
+++ assignment3/src/scripts/point.js
@@ -3,32 +3,30 @@
 
 /**
  * Constructor for Point, a simple data object with x and y coordinates.
+ * Representation Invariant: Immutable
  * @param {number} xCoordinate the x coordinate.
  * @param {number} yCoordinate the y coordinate.
  * @constructor
  * @const
  */
 othello.Point = function(xCoordinate, yCoordinate) {
-  /** @const */ this.xCoordinate = xCoordinate;
-  /** @const */ this.yCoordinate = yCoordinate;
-};
+  /**
+   * Getter for xCoordinate.
+   * @return {number} this.xCoordinate.
+   * @const
+   */
+ this.getX = function() {
+    return xCoordinate; 
+  };
 
 
-/**
- * Getter for xCoordinate.
- * @return {number} this.xCoordinate.
- */
-othello.Point.prototype.getX = function() {
-  return this.xCoordinate;
-};
-
-
-/**
- * Getter for yCoordinate.
- * @return {number} this.yCoordinate.
- */
-othello.Point.prototype.getY = function() {
-  return this.yCoordinate;
+  /**
+   * Getter for yCoordinate.
+   * @return {number} this.yCoordinate.
+   */
+  this.getY = function() {
+    return yCoordinate; 
+  }
 };
 
 
@@ -41,6 +39,6 @@ othello.Point.prototype.equals = function(other) {
   if (!(other instanceof othello.Point)) {
     return false;
   }
-  return this.xCoordinate === other.getX() &&
-         this.yCoordinate === other.getY();
+  return this.getX() === other.getX() &&
+         this.getY() === other.getY();
 };
