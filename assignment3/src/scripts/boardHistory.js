5b600073b3f853c31bea222f3c9f06c10f41c9e8
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-15 15:40:52 -0400

    Converted boardHistory to use closures

--- assignment3/src/scripts/boardHistory.js
+++ assignment3/src/scripts/boardHistory.js
@@ -4,70 +4,80 @@
 /**
  * A class to keep track of old boards for the undo/redo feature.
  * Representation invariant: Should always have at least one element.
+ * Closures are used to hide boards and boardIndex to enforce this.
  * @param {othello.Board} initialBoard the initial board.
  * @constructor
  * @const
  */
 othello.BoardHistory = function(initialBoard) {
-  this.boards = [initialBoard];
-  this.boardIndex = 0;
+  /** @const */ var boards = [initialBoard];
+  /** @const */ var boardIndex = 0;
+
+  
+  /**
+   * Push a board onto the history. This means throwing out
+   * any redos in front of the current board.
+   * @param {othello.Board} board the board to push.
+   * @const
+   */
+  this.push = function(board) {
+    /** @const */ var result =
+        _.map(_.range(0, boardIndex + 1), function(index) {
+      return boards[index];
+    });
+    result.push(board);
+    boards = result;
+    boardIndex = boards.length - 1;
+  };
+
+
+  /**
+   * Move back by one board. Return the board pointed at.
+   * @return {othello.Board} the previous board.
+   * @const
+   */
+  this.undo = function() {
+    boardIndex--;
+    return boards[boardIndex];
+  };
+
+
+  /**
+   * Verify that there are boards in the stack.
+   * @return {boolean} whether or not it's safe to undo.
+   * @const
+   */
+  this.canUndo = function() {
+    return boardIndex > 0;
+  };
+
+
+  /**
+   * Move forward by one baord.
+   * @return {othello.Board} the redone board.
+   * @const
+   */
+  this.redo = function() {
+    boardIndex++;
+    return boards[boardIndex];
+  };
+
+
+  /**
+   * Verify that it's possible to redo
+   * @return {boolean} whether it's safe  to redo.
+   */
+  this.canRedo = function() {
+    return (boardIndex + 1) < boards.length;
+  };
 };
 
 
-/**
- * Push a board onto the stack. This means throwing out
- * any redos in front of the current board.
- * @param {othello.Board} board the board to push.
- * @const
- */
-othello.BoardHistory.prototype.push = function(board) {
-  /** @const */ var self = this;
-  /** @const */ var result =
-      _.map(_.range(0, this.boardIndex + 1), function(index) {
-    return self.boards[index];
-  });
-  result.push(board);
-  this.boards = result;
-  this.boardIndex = this.boards.length - 1;
-};
 
 
-/**
- * Move back by one board. Return the board pointed at.
- * @return {othello.Board} the previous board.
- * @const
- */
-othello.BoardHistory.prototype.undo = function() {
-  this.boardIndex--;
-  return this.boards[this.boardIndex];
-};
 
 
-/**
- * Verify that there are boards in the stack.
- * @return {boolean} whether or not it's safe to undo.
- * @const
- */
-othello.BoardHistory.prototype.canUndo = function() {
-  return this.boardIndex > 0;
-};
 
 
-/**
- * Move forward by one baord.
- * @return {othello.Board} the redone board.
- * @const
- */
-othello.BoardHistory.prototype.redo = function() {
-  this.boardIndex++;
-  return this.boards[this.boardIndex];
-};
 
 
-/**
- * Verify that it's possible to redo
- * @return {boolean} whether it's safe  to redo.
- */
-othello.BoardHistory.prototype.canRedo = function() {
-  return (this.boardIndex + 1) < this.boards.length;
-};
