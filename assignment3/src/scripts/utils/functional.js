9082d1cbd92d156fcb36cf747ec5a121b7d5213d
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-14 21:39:21 -0400

    Added each to functional

--- assignment3/src/scripts/utils/functional.js
+++ assignment3/src/scripts/utils/functional.js
@@ -1,4 +1,17 @@
 /**
+ * Iterate through an array.
+ * @param {Array.<*>} array the array to walk.
+ * @param {function(*)} g the function to apply.
+ */
+othello.utils.each = function(array, g) {
+  for (var i = 0; i < array.length; i++) {
+    /** @const */ var element = array[i];
+    g(element);
+  }
+}
+
+
+/**
  * Given an array, applies g to all its elements,
  * then flattens the result.
  * @param {Array.<*>} array the array to walk, containing elements of type A.
@@ -8,9 +21,9 @@
  */
 othello.utils.flatMap = function(array, g) {
   /** @const */ var result = [];
-  _(array).each(function(item) {
+  othello.utils.each(array, function(item) {
     /** @const */ var gApplied = g(item);
-    _(gApplied).each(function(gApply) {
+    othello.utils.each(gApplied, function(gApply) {
       result.push(gApply);
     });
   });
