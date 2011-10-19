ec60db2d7e175221eb5db5dc1add903a92650a8b
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-19 12:20:57 -0400

    Updated compile.sh

--- assignment4/src/static/scripts/compile.sh
+++ assignment4/src/static/scripts/compile.sh
@@ -7,7 +7,11 @@
 
 java -jar /home/rahulraj/dls/closure-compiler/compiler.jar \
   --js=networkStickies.js --js=note.js \
+  --js=observable.js --js=observer.js \
+  --js=noteSet.js --js=noteModel.js \
+  --js=noteEditor.js \
   --js=noteController.js --js=noteView.js \
+  --js=main.js \
   --externs=externs.js --warning_level=VERBOSE \
   --compilation_level=SIMPLE_OPTIMIZATIONS \
   > /dev/null # We only care about the error output
