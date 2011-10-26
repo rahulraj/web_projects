51e9d9a082c0bb6f90b7e4d25b13f1e11e63b6df
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-26 17:06:54 -0400

    Copied over inject.py

---
+++ assignment5/linkshort/inject_test.py
@@ -0,0 +1,20 @@
+import unittest
+from inject import assign_injectables
+
+class TestAssignInjectables(unittest.TestCase):
+  def test_it_should_assign_variables_that_are_injected_into_the_object(self):
+    class WithInjectableConstructor(object):
+      def __init__(self, foo, bar):
+        assign_injectables(self, locals())
+
+    foo = 'foo'
+    bar = 'bar'
+
+    under_test = WithInjectableConstructor(foo, bar)
+
+    self.assertEquals(foo, under_test.foo, 'foo should be injected')
+    self.assertEquals(bar, under_test.bar, 'bar should also be injected')
+    self.assertFalse(hasattr(under_test, 'self'), 'self should not be injected')
+
+if __name__ == '__main__':
+  unittest.main()
