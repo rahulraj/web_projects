1cea8dacd42eae8fe199078adddfaa1f45038295
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-26 16:33:49 -0400

    DB initializer code

--- assignment5/linkshort/__init__.py
+++ assignment5/linkshort/__init__.py
@@ -1,8 +1,23 @@
-
-from flask import Flask, g, request, render_template, redirect, url_for,jsonify, json
+from contextlib import closing
+import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+app.secret_key = \
+  '\x8bj\xcaa\x82\x8e\xd1\xcb\x82\x8b\xdc\x93-\xa3\x0e\x12\x83=\x82xo\x8c\xe3A'
+app.database = 'link_shortener.db'
+
+def connect_database():
+  return sqlite3.connect(app.database)
+
+def initialize_database():
+  with closing(connect_database()) as database:
+    with app.open_resource('schema.sql') as schema:
+      database.cursor().executescript(schema.read())
+    database.commit()
+
 
 @app.before_request
 def before_request():
