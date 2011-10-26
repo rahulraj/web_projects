fa0ab54781120720744c000b37c61c22533b0dfe
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-26 16:33:35 -0400

    Fixed syntax errrors in the SQL

--- assignment5/linkshort/schema.sql
+++ assignment5/linkshort/schema.sql
@@ -1,23 +1,20 @@
 drop table if exists users;
 drop table if exists pages;
 drop table if exists page_visits;
-
 create table users (
-  id integer primary key autoincrement;
-  username string not null;
-  hashed_password string not null;
-  salt string not null;
+  id integer primary key autoincrement,
+  username string not null,
+  hashed_password string not null,
+  salt string not null
 );
-
 create table pages (
-  id integer primary key autoincrement;
-  created_by_user integer not null;
-  original_url string not null;
-  shortened_url string not null;
+  id integer primary key autoincrement,
+  created_by_user integer not null,
+  original_url string not null,
+  shortened_url string not null
 );
-
 create table page_visits (
-  id integer primary key autoincrement;
-  for_page integer not null;
-  time_visited integer not null;
+  id integer primary key autoincrement,
+  for_page integer not null,
+  time_visited integer not null
 );
