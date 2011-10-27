d51cf05fecadb6662c8a80f885790bf6cb073395
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-27 16:23:51 -0400

    Copied in users, and modified to use a sqlite3 database

---
+++ assignment5/linkshort/users_test.py
@@ -0,0 +1,52 @@
+import unittest
+from users import Users, confirmed_password_valid
+from databaseservice import User, NoSuchUser
+
+class FakeDatabaseService(object):
+  def __init__(self):
+    self.user_data = {}
+    self.row_counter = 0
+
+  def add_user(self, user):
+    username = user.get_username()
+    added_user = User(username, user.get_hashed_password(),
+        user.get_salt(), id=self.row_counter)
+    self.user_data[username] = added_user
+    self.row_counter += 1
+    return added_user
+
+  def has_user_with_name(self, username):
+    return username in self.user_data
+
+  def find_user_by_name(self, username):
+    if username not in self.user_data:
+      raise NoSuchUser
+    return self.user_data[username]
+
+class UsersTest(unittest.TestCase):
+  def setUp(self):
+    """ Using dicts instead of Shelve objects to speed up testing. """
+    self.fake_database = FakeDatabaseService()
+    self.users = Users(self.fake_database)
+    self.username = 'test_user'
+    self.password = 'test_password'
+
+  def test_confirm_passwords(self):
+    self.assertTrue(confirmed_password_valid('foo', 'foo'))
+    self.assertFalse(confirmed_password_valid('foo', 'bar'))
+
+  def test_simple_add_users(self):
+    self.assertFalse(self.users.has_user(self.username))
+    self.users.register_user(self.username, self.password)
+    self.assertTrue(self.users.has_user(self.username))
+    self.assertTrue(self.users.login_is_valid(self.username, self.password))
+
+  def test_login_before_register(self):
+    self.assertFalse(self.users.login_is_valid(self.username, self.password))
+
+  def test_login_with_wrong_password(self):
+    self.users.register_user(self.username, self.password)
+    self.assertFalse(self.users.login_is_valid(self.username, 'bad_password'))
+
+if __name__ == '__main__':
+  unittest.main()
