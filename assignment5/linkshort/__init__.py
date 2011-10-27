90944cf9f568c74e719fd8bba767a6b68f55dfee
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-10-27 16:01:22 -0400

    Added a page not found page

--- assignment5/linkshort/__init__.py
+++ assignment5/linkshort/__init__.py
@@ -17,19 +17,23 @@ def initialize_database():
 
 @app.before_request
 def before_request():
-    """ open dictionary/db connection"""
-    g.database = connect_database()
+  """ open dictionary/db connection"""
+  g.database = connect_database()
 
 
 @app.after_request
 def shutdown_session(response):
-    """ Closes the dictionary/db connection after each request """
-    g.database.close()
-    return response
+  """ Closes the dictionary/db connection after each request """
+  g.database.close()
+  return response
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
+@app.route('/<shortened_url>')
+def access_short_url(shortened_url):
+  return render_template('page-not-found.html', tried_url=shortened_url)
 
 if __name__ == '__main__':
   app.run(debug=True)
