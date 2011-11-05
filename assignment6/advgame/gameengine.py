d54f2b9cb5b4dfa778a88f02cd4b6604ef97d22e
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-11-05 15:39:02 -0400

    Added docstrings to GameEngie

--- assignment6/advgame/gameengine.py
+++ assignment6/advgame/gameengine.py
@@ -5,10 +5,30 @@ class UnknownItemType(Exception):
   pass
 
 class GameEngine(object):
+  """ Class to step through the game, prompting the user.  """
   def __init__(self, database_service, player_id):
+    """
+    Constructor for GameEngines
+
+    Args:
+      database_service the database that this depends on.
+      player_id the ID of the current player.
+    """
     assign_injectables(self, locals())
 
   def room_and_exits(self):
+    """
+    Query the database for the current room and its exits.
+    Does both in the same function because getting the exits
+    requires knowing the room, and querying for the room can
+    be an expensive operation. Therefore, if there was a separate
+    exits function, code that needs to know both items would end
+    up querying for the room twice.
+
+    Returns:
+      A (room, exits) tuple containing the Room in the first position,
+      and a list of Exits in the second position.
+    """
     current_room = self.database_service.find_room_occupied_by_player( \
         self.player_id)
     exits = self.database_service.find_exits_from_room_with_id( \
@@ -16,6 +36,12 @@ class GameEngine(object):
     return (current_room, exits)
 
   def inventory(self):
+    """
+    Finds the inventory for the player.
+
+    Returns:
+      A list of Items that the player has.
+    """
     return self.database_service.find_items_owned_by_player(self.player_id)
 
   def prompt(self):
@@ -51,6 +77,14 @@ class GameEngine(object):
     return prompt
 
   def possible_actions(self):
+    """
+    Retrieve the actions a player can do.
+
+    This can be inferred from the data in the database.
+
+    Returns:
+      A list of strings that would be valid input.
+    """
     (current_room, exits) = self.room_and_exits()
     # Go through an exit
     actions = ['exit ' + exit.get_name() for exit in exits]
@@ -64,6 +98,15 @@ class GameEngine(object):
     return actions
 
   def try_exit(self, exit_name):
+    """
+    Helper method to try to move the player through an exit, if it is unlocked.
+
+    Args:
+      exit_name the name of the exit.
+
+    Returns:
+      The message to send to the player.
+    """
     (_, exits) = self.room_and_exits()
     exit_in_use = [exit for exit in exits if exit.get_name() == exit_name][0]
     if exit_in_use.is_locked():
@@ -73,6 +116,15 @@ class GameEngine(object):
     return 'You went through ' + exit_in_use.get_name()
 
   def try_unlock_exit(self, item):
+    """
+    Helper method to try to use an item to unlock an exit.
+
+    Args:
+      item the item to use.
+
+    Returns:
+      A message explaining the outcome.
+    """
     (_, exits) = self.room_and_exits()
     unlockable_exits = [exit for exit in exits if \
         exit.get_id() == item.get_unlocks_exit()]
@@ -86,6 +138,13 @@ class GameEngine(object):
         """ % (item.get_name(), exit.get_name())
 
   def try_unlock_item(self, unlocking_item):
+    """
+    Try to unlock an item.
+
+    Args:
+      unlocking_item the item-unlocking item that may open up another
+      item in the current room.
+    """
     (room, _) = self.room_and_exits()
     locked_items  = self.database_service.find_locked_items_in_room( \
         room.get_id())
@@ -101,6 +160,15 @@ class GameEngine(object):
         """ % (unlocking_item.get_name(), to_unlock.get_name())
 
   def try_use_item(self, item_name):
+    """
+    Try to use an item. Delegates to the try_unlock_* functions.
+
+    Args:
+      item_name the name of the item to use.
+
+    Returns:
+      A message explaining the outcome.
+    """
     inventory = self.inventory()
     inventory_names = [item.get_name() for item in inventory]
     if item_name not in inventory_names:
@@ -114,6 +182,15 @@ class GameEngine(object):
       raise UnknownItemType
 
   def try_take_item(self, item_name):
+    """
+    Try to take an item from the current room
+
+    Args:
+      item_name the name of the item to take.
+
+    Returns:
+      A message explaining the outcome.
+    """
     (room, _) = self.room_and_exits()
     items = self.database_service.find_unlocked_items_in_room(room.get_id())
     item_names = [item.get_name() for item in items]
@@ -125,6 +202,15 @@ class GameEngine(object):
     return "You took the %s." % (item_name,)
 
   def step(self, action):
+    """
+    Execute one step throug the game.
+
+    Args:
+      action the user-inputted action to take.
+
+    Returns:
+      A message explaining the outcome.
+    """
     possible_actions = self.possible_actions()
     if action not in possible_actions:
       return "I don't know what you mean by " + action
